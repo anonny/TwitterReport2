915ddef74331c98469091cb90b10e030fc84cc02
anonny <sh4rk@sharklasers.com>
2015-03-02 00:27:45 +0000

    Update TwitterReport.py

--- TwitterReport.py
+++ TwitterReport.py
@@ -13,27 +13,27 @@ def main(argv):
         opts, args = getopt.getopt(argv,"hi:u:",["file=","user="])
     except getopt.GetoptError:
         print 'twitterReport.py -u <Twitter username> -i <file>'
-        print 'Le fichier des profiles doit comporter une URL par ligne ++'
+        print 'The Twitter accounts list must have one URL per line ++'
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
             print 'twitterReport.py -u <Twitter username> -i <file.txt>'
-            print 'Le fichier des profiles doit comporter une URL par ligne'
+            print 'The Twitter accounts list must have one URL per line ++'
             sys.exit()
         elif opt in ("-i", "--file"):
             txt = arg
         elif opt in ("-u", "--user"):
             username = arg
 
-    password = raw_input("Enter your twitter password : ")
+    password = raw_input("Enter your Twitter password: ")
 
     try:
         username
         txt
     except getopt.GetoptError:
         print 'twitterReport.py -u <Twitter username> -i <file>'
-        print 'Le fichier des profiles doit comporter une URL par ligne'
+        print 'The Twitter accounts list must have one URL per line ++'
         sys.exit()
 
     with Browser() as browser:
@@ -44,7 +44,7 @@ def main(argv):
         try:
             file = open(txt, 'r')
         except:
-            print "Impossible d'ouvrir le fichier"
+            print "Unable to open file"
 
         for line in file:
             try:
@@ -53,10 +53,17 @@ def main(argv):
                 browser.visit(url)
                 if not browser.is_element_present_by_css('.route-account_suspended'):
                     browser.find_by_css('.user-dropdown').click()
-                    browser.find_by_css('li.block-or-report-text button[type="button"]').click()
-                    browser.uncheck('block_user')
-                    browser.check('also_report')
-                    browser.find_by_css('.report-tweet-report-button').click()
+                    reportie = browser.find_by_css('li.report-text.pretty-link')
+                    reportie.find_by_css('.dropdown-link').click()
+                    browser.find_by_xpath('//*[@id="report-dialog-dialog"]/div[2]/div[3]/div[1]/fieldset/label[4]/span').click()
+                    browser.find_by_xpath('//*[@id="report-dialog-dialog"]/div[2]/div[3]/div[1]/fieldset/label[4]/fieldset/table/tbody/tr[1]/td[3]/label/input').click()
+                    browser.find_by_xpath('//*[@id="report-control"]/div/button[1]').last.click()
+                    with browser.get_iframe(2) as iframe:
+                        iframe.choose('victim','Someone_else')
+                    browser.find_by_css('.harassment-next-button').last.click()
+                    with browser.get_iframe(2) as iframe:
+                        iframe.choose('detail','harassed')
+                    browser.find_by_xpath('//*[@id="abuse-harassment-control"]/div/button[1]').last.click()
                     followers = browser.find_by_css('a[data-nav="followers"] .ProfileNav-value').value;
                     msg = url.strip()+' - ' + followers + ' Followers'
                     with open("log_reported_"+date+".txt", "a") as log:
